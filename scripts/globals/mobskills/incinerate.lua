e412f6214586334e5fefd3bf0f0e34fb9f9feb9e
Gweivyth Quartet <admin@mageringnetwork.com>
2021-07-14 00:09:15 -0400

    Adds HP% check to Eruca move incinerate.lua and also slightly lowers its power, it was consistently hitting players for their full HP bar.

--- scripts/globals/mobskills/incinerate.lua
+++ scripts/globals/mobskills/incinerate.lua
@@ -12,8 +12,10 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = MobBreathMove(mob, target, 0.25, 0.75, tpz.magic.ele.FIRE)
+    local dmgmod = MobBreathMove(mob, target, 0.05, 0.15, tpz.magic.ele.FIRE)
     local dmg = MobFinalAdjustments(dmgmod, mob, skill, target, tpz.attackType.BREATH, tpz.damageType.FIRE, MOBPARAM_IGNORE_SHADOWS)
+	
+	dmgmod = 1.5 + skill:getMobHPP()/10
 
     target:takeDamage(dmg, mob, tpz.attackType.BREATH, tpz.damageType.FIRE)
     return dmg
